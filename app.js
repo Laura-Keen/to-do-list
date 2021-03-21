361256cd9fad559ffdb6e03929dd21aac8bf01d7
Laura <keenlaura@hotmail.co.uk>
2021-03-21 14:17:39 +0000

    Set up selectors in js file

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButtton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
 
-//Event Listeners
+// Event Listeners
 
-//Funcitons
+// Functions
